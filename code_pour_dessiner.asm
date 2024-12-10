4c9a89369d396225f4ccdc72c66c5c3a1a7ae239
Ptitmorceaux <106253875+ptitmorceaux@users.noreply.github.com>
2024-12-10 12:15:57 +0100

    init projet

---
+++ code_pour_dessiner.asm
@@ -0,0 +1,232 @@
+; external functions from X11 library
+extern XOpenDisplay
+extern XDisplayName
+extern XCloseDisplay
+extern XCreateSimpleWindow
+extern XMapWindow
+extern XRootWindow
+extern XSelectInput
+extern XFlush
+extern XCreateGC
+extern XSetForeground
+extern XDrawLine
+extern XNextEvent
+extern XDrawArc
+
+; external functions from stdio library (ld-linux-x86-64.so.2)    
+extern printf
+extern exit
+
+%define	StructureNotifyMask	131072
+%define KeyPressMask		1
+%define ButtonPressMask		4
+%define MapNotify		19
+%define KeyPress		2
+%define ButtonPress		4
+%define Expose			12
+%define ConfigureNotify		22
+%define CreateNotify 16
+%define QWORD	8
+%define DWORD	4
+%define WORD	2
+%define BYTE	1
+
+%define WIDTH	1200
+%define HEIGHT	1200
+
+global main
+
+section .bss
+display_name:	resq	1
+screen:			resd	1
+depth:         	resd	1
+connection:    	resd	1
+width:         	resd	1
+height:        	resd	1
+window:		resq	1
+gc:		resq	1
+
+section .data
+
+event:		times	24 dq 0
+
+
+section .text
+	
+;##################################################
+;########### PROGRAMME PRINCIPAL ##################
+;##################################################
+
+main:
+;###########################################################
+; Mettez ici votre code qui devra s'exécuter avant le dessin
+;###########################################################
+
+
+
+
+
+;###############################
+; Code de création de la fenêtre
+;###############################
+xor     rdi,rdi
+call    XOpenDisplay	; Création de display
+mov     qword[display_name],rax	; rax=nom du display
+
+; display_name structure
+; screen = DefaultScreen(display_name);
+mov     rax,qword[display_name]
+mov     eax,dword[rax+0xe0]
+mov     dword[screen],eax
+
+mov rdi,qword[display_name]
+mov esi,dword[screen]
+call XRootWindow
+mov rbx,rax
+
+mov rdi,qword[display_name]
+mov rsi,rbx
+mov rdx,10
+mov rcx,10
+mov r8,WIDTH	; largeur
+mov r9,HEIGHT	; hauteur
+push 0xFFFFFF	; background  0xRRGGBB
+push 0x00FF00
+push 1
+call XCreateSimpleWindow
+mov qword[window],rax
+
+mov rdi,qword[display_name]
+mov rsi,qword[window]
+mov rdx,131077 ;131072
+call XSelectInput
+
+mov rdi,qword[display_name]
+mov rsi,qword[window]
+call XMapWindow
+
+mov rsi,qword[window]
+mov rdx,0
+mov rcx,0
+call XCreateGC
+mov qword[gc],rax
+
+mov rdi,qword[display_name]
+mov rsi,qword[gc]
+mov rdx,0x000000	; Couleur du crayon
+call XSetForeground
+
+boucle: ; boucle de gestion des évènements
+mov rdi,qword[display_name]
+mov rsi,event
+call XNextEvent
+
+cmp dword[event],ConfigureNotify	; à l'apparition de la fenêtre
+je dessin							; on saute au label 'dessin'
+
+cmp dword[event],KeyPress			; Si on appuie sur une touche
+je closeDisplay						; on saute au label 'closeDisplay' qui ferme la fenêtre
+;jmp boucle
+
+;#########################################
+;#		DEBUT DE LA ZONE DE DESSIN		 #
+;#########################################
+dessin:
+
+;couleur du cercle 1
+mov rdi,qword[display_name]
+mov rsi,qword[gc]
+mov edx,0xFF0000	; Couleur du crayon ; rouge
+call XSetForeground
+
+; Dessin du cercle 1
+mov rdi,qword[display_name]
+mov rsi,qword[window]		
+mov rdx,qword[gc]			
+mov bx,400	; COORDONNEE en X DU CERCLE
+mov cx,200	; RAYON DU CERCLE
+sub bx,cx				
+movzx rcx,bx			
+mov bx,300	; COORDONNEE en Y DU CERCLE
+mov r15w,200	; RAYON DU CERCLE
+sub bx,r15w
+movzx r8,bx		
+mov r9,200	; RAYON DU CERCLE
+shl r9,1
+mov rax,23040
+push rax
+push 0
+push r9
+call XDrawArc
+
+;couleur du cercle 2
+mov rdi,qword[display_name]
+mov rsi,qword[gc]
+mov edx,0x00FF00	; Couleur du crayon ; vert
+call XSetForeground
+
+; Dessin du cercle 2
+mov rdi,qword[display_name]
+mov rsi,qword[window]		
+mov rdx,qword[gc]			
+mov bx,200	; COORDONNEE en X DU CERCLE
+mov cx,100 ; RAYON DU CERCLE
+sub bx,cx
+movzx rcx,bx
+mov bx,400	; COORDONNEE en Y DU CERCLE
+mov r15w,100	; RAYON DU CERCLE
+sub bx,r15w
+movzx r8,bx		
+mov r9,100	; RAYON DU CERCLE
+shl r9,1
+mov rax,23040
+push rax
+push 0
+push r9
+call XDrawArc
+
+;couleur du cercle 3
+mov rdi,qword[display_name]
+mov rsi,qword[gc]
+mov edx,0x0000FF	; Couleur du crayon ; vert
+call XSetForeground
+
+; Dessin du cercle 3
+mov rdi,qword[display_name]
+mov rsi,qword[window]		
+mov rdx,qword[gc]			
+mov bx,50	; COORDONNEE en X DU CERCLE
+mov cx,250 ; RAYON DU CERCLE
+sub bx,cx				
+movzx rcx,bx			
+mov bx,150	; COORDONNEE en Y DU CERCLE
+mov r15w,250	; RAYON DU CERCLE
+sub bx,r15w
+movzx r8,bx		
+mov r9,250	; RAYON DU CERCLE
+shl r9,1
+mov rax,23040
+push rax
+push 0
+push r9
+call XDrawArc
+
+; ############################
+; # FIN DE LA ZONE DE DESSIN #
+; ############################
+;jmp flush
+
+flush:
+mov rdi,qword[display_name]
+call XFlush
+jmp boucle
+mov rax,34
+syscall
+
+closeDisplay:
+    mov     rax,qword[display_name]
+    mov     rdi,rax
+    call    XCloseDisplay
+    xor	    rdi,rdi
+    call    exit
+	
